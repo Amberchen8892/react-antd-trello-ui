dafc1610c0cb69d3660bba70c9572c1898345310
your Name <myphuong8892@gmail.com>
2023-01-07 23:25:11 -0700

    install react drag n drop, mock data and divide code into components

---
+++ src/data.js
@@ -0,0 +1,56 @@
+// hash map object
+  const data= {
+    columns: ["list-1", "list-2"],
+    lists: {
+      "list-1": {
+        id: "list-1",
+        title: "list 1",
+        cards: ["card-1", "card-2"],
+      },
+      "list-2": {
+        id: "list-2",
+        title: "list 2",
+        cards: ["card-3"],
+      },
+    },
+    cards: [
+      {
+        id: "card-1",
+        title: "card 1",
+        description: "",
+        member: [
+          {
+            id: "member-1",
+            name: "member 1",
+          },
+        ],
+        status: "NEW",
+      },
+      {
+        id: "card-2",
+        title: "card 1",
+        description: "",
+        member: [
+          {
+            id: "member-1",
+            name: "member 1",
+          },
+        ],
+        status: "NEW",
+      },
+      {
+        id: "card-3",
+        title: "card 1",
+        description: "",
+        member: [
+          {
+            id: "member-1",
+            name: "member 1",
+          },
+        ],
+        status: "NEW",
+      },
+    ],
+  };
+
+  export default data;
